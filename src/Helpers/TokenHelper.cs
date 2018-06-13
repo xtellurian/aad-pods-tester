d6ad7496a977bbba14e9d5ce68fbd4e9a96d5902
Rian Finnegan <flanagan89@gmail.com>
2018-06-13 14:10:28 +1000

    initial commit with code that gets resource groups

---
+++ src/Helpers/TokenHelper.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Management.ResourceManager;
+using Microsoft.Azure.Management.ResourceManager.Models;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Rest;
+using Newtonsoft.Json;
+
+namespace src
+{
+
+    public static class TokenHelper
+    {
+        public static async Task<string> TestMethod()
+        {
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+            return JsonConvert.SerializeObject(await GetResourceGroups(azureServiceTokenProvider));
+        }
+        private static async Task GetSecretFromKeyVault(AzureServiceTokenProvider azureServiceTokenProvider)
+        {
+            KeyVaultClient keyVaultClient =
+                new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+            Console.WriteLine("Please enter the key vault name");
+
+            var keyVaultName = "gotta add this";
+
+            try
+            {
+                var secret = await keyVaultClient
+                    .GetSecretAsync($"https://{keyVaultName}.vault.azure.net/secrets/secret")
+                    .ConfigureAwait(false);
+
+                Console.WriteLine($"Secret: {secret.Value}");
+
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine($"Something went wrong: {exp.Message}");
+            }
+        }
+
+        private static async Task<IEnumerable<ResourceGroup>> GetResourceGroups(AzureServiceTokenProvider azureServiceTokenProvider)
+        {
+            Console.WriteLine($"{Environment.NewLine}{Environment.NewLine}Please enter the subscription Id");
+
+            var subscriptionId = "c5760548-23c2-4223-b41e-5d68a8320a0c";
+
+            try
+            {
+                var serviceCreds = new TokenCredentials(await azureServiceTokenProvider.GetAccessTokenAsync("https://management.azure.com/").ConfigureAwait(false));
+
+                var resourceManagementClient =
+                    new ResourceManagementClient(serviceCreds) { SubscriptionId = subscriptionId };
+
+                var resourceGroups = await resourceManagementClient.ResourceGroups.ListAsync();
+
+                foreach (var resourceGroup in resourceGroups)
+                {
+                    Console.WriteLine($"Resource group {resourceGroup.Name}");
+                }
+                return resourceGroups;
+
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine($"Something went wrong: {exp.Message}");
+            }
+            return null;
+        }
+    }
+}
